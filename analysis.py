03becfe3b80499723beb8a686c7175f2474ce67c
Xizhao Deng <xizhaodeng@berkeley.edu>
2015-11-11 19:56:39 -0800

    p3 not done

--- analysis.py
+++ analysis.py
@@ -33,9 +33,9 @@ def question3a():
     # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.9
+    answerNoise = 0.3
+    answerLivingReward = -2
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
